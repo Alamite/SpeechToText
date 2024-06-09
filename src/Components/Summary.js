90343c683a9b3aff3d3feaf7869046ee21e66207
alamite <adi.palamittam@gmail.com>
2024-06-09 22:03:58 +0530

    summary

--- src/Components/Summary.js
+++ src/Components/Summary.js
@@ -1,6 +1,28 @@
+import React from 'react';
+import '../styles.css';
+import jsonData from '../Data/TranscriptOutput1.json';
 
 function Summary() {
-    return(<div>Summary</div>);
+    const { summaries } = jsonData;
+    // Remove the first hyphen and split the cohere points into an array
+    const coherePoints = summaries.cohere_points.startsWith('- ')
+        ? summaries.cohere_points.substring(2).split('\n- ').filter(point => point.trim() !== '')
+        : summaries.cohere_points.split('\n- ').filter(point => point.trim() !== '');
+
+    return (
+        <div className='transcript-content'>
+            <h2>Overall Summary</h2>
+            <p>{summaries.overall_summary}</p>
+
+
+            <h3>Sequence of events</h3>
+            <ul>
+                {coherePoints.map((point, index) => (
+                    <li key={index}>{point}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Summary;
