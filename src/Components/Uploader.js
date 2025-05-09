1760b62f060954963c991b79d89facefb8ca25fb
Alamite <69043698+Alamite@users.noreply.github.com>
2025-05-09 12:19:32 +0530

    Update Uploader.js

--- src/Components/Uploader.js
+++ src/Components/Uploader.js
@@ -17,8 +17,8 @@ function Uploader({ startTime, clicked, onTimeUpdate, onSelectFile, audioFile })
     const [files, setFiles] = React.useState([]);
 
     useEffect(() => {
-        fetch('https://inscribe.3cctpl.co.in/api/files')   
-        // fetch('http://localhost:3001/api/files')
+        //fetch('https://inscribe.3cctpl.co.in/api/files')   
+        fetch('http://localhost:3001/api/files')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
